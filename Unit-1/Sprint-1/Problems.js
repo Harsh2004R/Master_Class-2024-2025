1c1f40299cc0463d0cbdee91d26842ecd5092a99
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-11-26 12:33:43 -0500

    Problem -2 solved U-1 S1

--- Unit-1/Sprint-1/Problems.js
+++ Unit-1/Sprint-1/Problems.js
@@ -6,12 +6,32 @@
 
 // Solution Problem - 1
 
-let arr = [2, 5, 6, 6, 8, 7, 7, 10];
-let bag = "";
-for (let i = 0; i < arr.length; i++) {
-    if (i%2!==0){
-        bag += arr[i] + " ";
-    }
-}
-console.log(bag)
+// let arr = [2, 5, 6, 6, 8, 7, 7, 10];
+// let bag = "";
+// for (let i = 0; i < arr.length; i++) {
+//     if (i%2!==0){
+//         bag += arr[i] + " ";
+//     }
+// }
+// console.log(bag)
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Pronblem - 2
+// Given an array find out the number of elements for which both the neighbor are greater than the element.
+// Input = [5,7,6,9,8,10];
+// Output expected : 6 8
+
+
+// Solution Problem - 2
+
+// let array = [5,7,6,9,8,10];
+// let bag = "";
+// for (let i = 1; i<array.length-1; i++){
+//     if (array[i]<array[i-1] && array[i]<array[i+1]){
+//         bag+=array[i] + " ";
+//     }
+// }
+// console.log(bag)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
